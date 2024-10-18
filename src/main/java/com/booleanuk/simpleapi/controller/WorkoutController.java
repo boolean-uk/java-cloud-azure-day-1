da3f6753faa59dd459d20704c2340b3be357762b
Stian Rusvik <stian.rusvik@no.experis.com>
2024-10-18 11:08:00 +0200

    implemented first version of controller.

--- src/main/java/com/booleanuk/simpleapi/controller/WorkoutController.java
+++ src/main/java/com/booleanuk/simpleapi/controller/WorkoutController.java
@@ -1,7 +1,61 @@
+
 package com.booleanuk.simpleapi.controller;
 
-import org.springframework.web.bind.annotation.RestController;
+import com.booleanuk.simpleapi.model.Workout;
+import com.booleanuk.simpleapi.repository.WorkoutRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
+@RequestMapping("v1/workouts")
 public class WorkoutController {
+    private final WorkoutRepository workoutRepository;
+
+    @Autowired
+    public WorkoutController(WorkoutRepository workoutRepository) {
+        this.workoutRepository = workoutRepository;
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Workout>> getAllWorkouts() {
+        return ResponseEntity.ok(this.workoutRepository.findAll());
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Workout> getWorkoutById(@PathVariable int id) {
+        return this.workoutRepository.findById(id)
+                .map(ResponseEntity::ok)
+                .orElse(ResponseEntity.notFound().build());
+    }
+
+    @PostMapping
+    public ResponseEntity<Workout> createWorkout(@RequestBody Workout workout) {
+        return ResponseEntity.ok(this.workoutRepository.save(workout));
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Workout> updateWorkout(@PathVariable int id, @RequestBody Workout workoutBody) {
+        return this.workoutRepository.findById(id)
+                .map(workout -> {
+                    workout.setWorkoutType(workoutBody.getWorkoutType());
+                    workout.setWorkoutDate(workoutBody.getWorkoutDate());
+                    workout.setExercises(workoutBody.getExercises());
+                    Workout updatedWorkout = this.workoutRepository.save(workout);
+                    return ResponseEntity.ok(updatedWorkout);
+                })
+                .orElse(ResponseEntity.notFound().build());
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Object> deleteWorkout(@PathVariable int id) {
+        return this.workoutRepository.findById(id)
+                .map(workout -> {
+                    this.workoutRepository.delete(workout);
+                    return ResponseEntity.noContent().build();
+                })
+                .orElse(ResponseEntity.notFound().build());
+    }
 }
