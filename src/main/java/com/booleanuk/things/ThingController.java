db553a5390ddb511e48b1141cf151b02cdabf85f
gremble0 <hermanstornes@gmail.com>
2024-10-18 15:38:16 +0200

    Add remaining crud endpoints to thingcontroller

--- src/main/java/com/booleanuk/things/ThingController.java
+++ src/main/java/com/booleanuk/things/ThingController.java
@@ -4,9 +4,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
 
+@CrossOrigin
 @RestController
 @RequestMapping("things")
 public class ThingController {
@@ -18,10 +20,34 @@ public class ThingController {
     return ResponseEntity.ok(this.repository.findAll());
   }
 
+  @GetMapping("{id}")
+  public ResponseEntity<Thing> get(@PathVariable int id) {
+    return ResponseEntity.ok(this.repository.findById(id)
+        .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND)));
+  }
+
   @PostMapping
   public ResponseEntity<Thing> post(@RequestBody Thing thing) {
     System.out.println(thing);
     return ResponseEntity.status(HttpStatus.CREATED)
         .body(this.repository.save(thing));
+  }
+
+  @PutMapping("{id}")
+  public ResponseEntity<Thing> put(@PathVariable int id, @RequestBody Thing thing) {
+    return ResponseEntity.ok(this.repository.findById(id)
+        .map(existing -> {
+          existing.setName(thing.getName());
+          existing.setPrice(thing.getPrice());
+          existing.setProducer(thing.getProducer());
+          return existing;
+        }).orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND)));
+  }
+
+  @DeleteMapping("{id}")
+  public ResponseEntity<Thing> delete(@PathVariable int id) {
+    var deleted = this.repository.findById(id).orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND));
+    this.repository.deleteById(id);
+    return ResponseEntity.ok(deleted);
   }
 }
